9b305d97d0566d7d89978c4353eca81490e0485d
JiYoonKimjimmy <jimmyberg.kim@gmail.com>
2020-09-15 12:20:15 +0900

    properties 분리 관련 수정

--- src/main/java/com/demo/restapi/controller/common/ProfileController.java
+++ src/main/java/com/demo/restapi/controller/common/ProfileController.java
@@ -3,13 +3,15 @@ package com.demo.restapi.controller.common;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController(value = "/profile")
+@RestController
+@RequestMapping(value = "/profile")
 public class ProfileController {
 
     private final Environment environment;
